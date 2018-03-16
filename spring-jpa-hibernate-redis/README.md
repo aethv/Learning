ea3317891afd1392607e9782f827ca13e1e69a65
Thang Nguyen <k.mendo87@gmail.com>
2018-03-16 17:32:00 +0700

    Init redis template

---
+++ spring-jpa-hibernate-redis/README.md
@@ -0,0 +1,8 @@
+Demo project using:
+> Spring boot
+
+> Spring JPA
+
+> Hibernate
+
+> Redis
---
+++ spring-jpa-hibernate-redis/src/main/java/info/kzthink/springjpahibernateredis/SpringJpaHibernateRedisApplication.java
@@ -0,0 +1,12 @@
+package info.kzthink.springjpahibernateredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaHibernateRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaHibernateRedisApplication.class, args);
+	}
+}
